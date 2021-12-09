f56ddfbd9c8e62532795700e2d1141d66194095e
Ahmed Muhammad <ahmedm357@gmail.com>
2021-12-09 09:28:55 -0500

    handling args in safe_decospector so that mutate and find work (although a bit convoluted now

--- decospectors/decospectors.py
+++ decospectors/decospectors.py
@@ -10,7 +10,6 @@ from typing import (
     NamedTuple,
     NoReturn,
     Optional,
-    Union,
 )
 
 from .utils import (
@@ -33,24 +32,50 @@ class _Decospector:
         # categorizing parameters
         self.sig = inspect.signature(func)
         self.params = self.sig.parameters
+        self.param_kinds = defaultdict(list)
+        for p in self.params:
+            param_kind = self.params[p].kind.name
+            self.param_kinds[param_kind].append(p)
 
         # mapping arguments to their parameter names
         self.binding = self.sig.bind(*args, **kwargs)
         self.binding.apply_defaults()
         self.mapped_args = self.binding.arguments.copy()
 
-    def expect_forced_params(self) -> bool:
-        return any(
-            param_kind.endswith('_ONLY')
-            for param_kind in
-            [self.params[p].kind.name for p in self.params]
-        )
+    def check_param_kinds(self, check_word: str) -> list[str]:
+        matching_params = [self.param_kinds[k] for k in self.param_kinds.keys()
+                           if check_word in k]
+        # flatten that list
+        return [param for sublist in matching_params for param in sublist]
 
-    def get_positional_params(self,
-                              kind: Literal['POSITIONAL', 'POSITIONAL_ONLY']
-                              ) -> list[str]:
-        return [p for p in self.params
-                if self.params[p].kind.name == kind]
+    def unload_args(self, preserve_defaults: bool) -> dict:
+        params, mapped_args, args = self.params, self.mapped_args, list(self._args)
+        try:
+            vararg_param, = self.param_kinds['VAR_POSITIONAL']
+        except ValueError:
+            # nothing to unload!
+            return mapped_args
+        empty = inspect.Parameter.empty
+
+        unloading_params = {p: params[p].default
+                            for p in self.check_param_kinds('POSITIONAL')
+                            if p != vararg_param}
+
+        unloaded = defaultdict(list)
+        # all positional args are loading varargs, so they will be unpacked
+        for i, (param, def_value) in enumerate(unloading_params.items()):
+            if def_value is not empty and preserve_defaults:
+                # keep default value
+                unloaded[param] = def_value
+            else:
+                unloaded[param] = args[i]  # pop from varargs
+
+        unloaded[vararg_param] += args[i:]  # unpack remaining varargs
+        return {
+            kw: unloaded[kw]
+            if kw in unloaded.keys() else keep_kwargified_value
+            for kw, keep_kwargified_value in mapped_args.items()
+        }
 
     def map_args_to_params(self,
                            preserve_positional_defaults: bool = True,
@@ -60,48 +85,23 @@ class _Decospector:
         empty = inspect.Parameter.empty
         args, kwargs, sig, params, mapped_args = self._get_attributes()
 
-        pos_params = {p: params[p].default for p in
-                      filter(lambda _: 'POSITIONAL' in params[_].kind.name, params)
-                      if p != enumerate_varargs}
-        # three conditions
-        # 1. user wants to override python default behavior
-        if ( preserve_positional_defaults
-             # 2. positional defaults have been given
-             and any( filter(lambda _: _ is not empty, pos_params) )
-             # 3. variadic arguments given
-             and 'VAR_POSITIONAL' in [params[p].kind.name for p in params] ):
+        mapped_args = self.unload_args(preserve_positional_defaults)
 
-            resolved = defaultdict(list)
-            for i, param in enumerate(pos_params.keys()):
-                if (value := pos_params[param]) is not None:
-                    resolved[param] = value
-                else:
-                    resolved[param] = args[i]
-
-            resolved[enumerate_varargs] += args[i:]  # remaining args
-            # modify kwargified in place with defaults
-            mapped_args = {
-                kw: default_value
-                if kw in resolved.keys()
-                and (default_value := resolved[kw]) is not empty
-                else keep_kwargified_value
-                for kw, keep_kwargified_value in mapped_args.items()
-            }
-
-        for expected in (enumerate_varargs, merge_varkwargs):
-            if not expected or expected not in mapped_args:
+        # enumerate varargs or merge varkwargs
+        for expectation in (enumerate_varargs, merge_varkwargs):
+            if not expectation or expectation not in mapped_args:
                 continue
 
-            values = mapped_args.pop(expected)
+            values = mapped_args.pop(expectation)
             mapped_args |= (
-                {f'{expected}{i}': val for i, val in enumerate(values)}  # args
+                {f'{expectation}{i}': val for i, val in enumerate(values)}  # args
                  if not isinstance(values, dict) else
                 values  # kwargs
             )
         return mapped_args
 
     def _get_attributes(self) -> Generator:
-        return_props = (self._args,
+        return_props = (list(self._args),
                         self._kwargs,
                         self.sig,
                         self.params,
@@ -129,61 +129,71 @@ def safe_decospector(func: Callable,
                      _merge_varkwargs_: Optional[str] = 'kwargs',
                      **kwargs: Any) -> tuple[ValuesDict, dict]:
     self = _Decospector(func, *args, **kwargs)
-    params = self.params
     mapped_args = self.map_args_to_params(_preserve_positional_defaults_,
-                                          _enumerate_varargs_,
+                                          _enumerate_varargs_,  # don't enumerate args
                                           _merge_varkwargs_)
 
-    if not self.expect_forced_params():
+    pos: ValuesDict
+    nonpos: dict
+    if not self.check_param_kinds('_ONLY'):
         pos, nonpos = PargsKwargs({}, mapped_args)
 
     else:
-        _positionals = {param: mapped_args[param]
-                        for param in self.get_positional_params('POSITIONAL_ONLY')}
-        _remaining_kwargs = {k: v
-                             for k, v in mapped_args.items()
-                             if k not in _positionals}
+        pos = {param: mapped_args[param]
+               for param in self.param_kinds['POSITIONAL_ONLY']}
+        nonpos = {k: v for k, v in mapped_args.items() if k not in pos}
 
-        pos, nonpos = PargsKwargs(ValuesDict(**_positionals), _remaining_kwargs)
+    # move default arguments to positionals dict
+    if _preserve_positional_defaults_:
+        for param in self.check_param_kinds('POSITIONAL_OR_KEYWORD'):
+            pos[param] = nonpos.pop(param)
 
-    # add default positional arguments and variadic arguments to positionals dict
-    varargs = [match.group() for arg in mapped_args
-               if (match := re.match(f'{_enumerate_varargs_}\\d+', arg))]
-    if ( _preserve_positional_defaults_ and
-         any(varargs) ):
-        for param in self.get_positional_params('POSITIONAL_OR_KEYWORD'):
-            pos[param] = nonpos.pop(param)  # saving defaults
+    # move from keywords dict to positionals dict
+    try:
+        vararg_param, = self.check_param_kinds('VAR_POS')
+        varargs = [match.group() for arg in mapped_args
+                   if (match := re.match(f'{vararg_param}\\d+', arg))]
+
         for vararg in varargs:
             pos[vararg] = nonpos.pop(vararg)
 
-    def context() -> PargsKwargs: return PargsKwargs(pos, nonpos)
+    except ValueError:
+        # no varargs!
+        pass
+
+    def context() -> PargsKwargs:
+        """"""
+        return PargsKwargs(ValuesDict(**pos), nonpos)
 
     def find_param(param: str,
                    get_code: bool = False,
-                   ) -> Union[Union[tuple[SafeCode, Any], Any], NoReturn]:
-        if param in pos.keys():
-            return (
-                (SafeCode.POSITIONAL, pos[param])
-                if get_code else
-                pos[param]
-            )
+                   ) -> tuple[SafeCode, Any | list] | Any | list | NoReturn:
+        if param in self.check_param_kinds('VAR_') and _enumerate_varargs_:
+            _args = [p for p in pos.keys() if p.startswith(vararg_param)]
+            return (SafeCode.VARARG, _args) if get_code else _args
+
+        elif param in pos.keys():
+            return (SafeCode.POSITIONAL, pos[param]) if get_code else pos[param]
 
         elif param in nonpos.keys():
-            return (
-                (SafeCode.NONPOSITIONAL, nonpos[param])
-                if get_code else
-                nonpos[param]
-            )
+            return (SafeCode.NONPOSITIONAL, nonpos[param]) if get_code else nonpos[param]
 
         else:
             raise KeyError(f'"{param}" param does not exist. Available params: '
                            f"{list(pos.keys())}, {list(nonpos.keys())}")
 
-    def mutate(param: str, new_value: Any) -> Union[SafeCode, NoReturn]:
-        code, _ = find_param(param, get_code=True)
+    def mutate(param: str, new_value: Any) -> SafeCode:
+        code, found_param = find_param(param, get_code=True)
 
         if code == SafeCode.POSITIONAL:
             pos[param] = new_value
+
+        elif code == SafeCode.VARARG:
+            for i, param in enumerate(found_param):
+                pos[param] = new_value[i]
+            # this raised UnboundLocalError for some reason:
+            # pos |= dict(zip(found_param, new_value))
+
         else:
             nonpos[param] = new_value
         return code
